6039d28c8a5e9b0e0f756e681852680e047cf386
lalala-233 <2317987274@qq.com>
2023-03-01 23:05:03 +0800

    build: 完善构建工具选项

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,9 @@
-add_rules("mode.debug", "mode.release")
-
 set_project("my_cpp_test")
 
+add_rules("mode.debug", "mode.release")
+
 set_languages("c++23")
+set_policy("build.warning", true)
 add_cxxflags("-Wall", "-Wconversion", "-Weffc++", "-Wextra", "-Wpedantic", "-Wshadow", "-Wunused")
 
 includes("project/**/xmake.lua")
