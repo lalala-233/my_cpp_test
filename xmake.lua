921d30809771919ec3694b8bdf05ac907ab0ee69
lalala-233 <2317987274@qq.com>
2023-03-22 13:53:59 +0800

    build: 调整 xmake 编译设置

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,11 @@
-set_project("my_cpp_test")
-
-add_rules("mode.debug", "mode.release")
-
-set_languages("c++23")
-set_policy("build.warning", true)
 add_cxxflags("-Wall", "-Wconversion", "-Weffc++", "-Wextra", "-Wpedantic", "-Wshadow", "-Wunused")
 
 includes("project/**/xmake.lua")
+
+set_languages("c++23")
+
+set_policy("build.warning", true)
+
+set_project("my_cpp_test")
+
+add_rules("mode.debug", "mode.release")
